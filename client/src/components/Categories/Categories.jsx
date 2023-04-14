97072f007b1380699c955bfee86917332b99a30b
ga7axy <akshaymrajput@gmail.com>
2023-04-14 23:53:55 +0530

    added categories section, might change the design later, need to update links

--- client/src/components/Categories/Categories.jsx
+++ client/src/components/Categories/Categories.jsx
@@ -6,22 +6,52 @@ const Categories = () => {
   return (
     <div className="categories">
       <div className="col">
-        <div className="row">r1</div>
-        <div className="row">r2</div>
+        <div className="row">
+          <div className="cb-1"></div>
+          <Link className="link" to="/products/1">
+            <button>Sale</button>
+          </Link>
+        </div>
+        <div className="row">
+          <div className="cb-2"></div>
+          <Link className="link" to="/products/1">
+            <button>New Arrivals</button>
+          </Link>
+        </div>
       </div>
       <div className="col">
-        <div className="row">r3</div>
+        <div className="row">
+          <div className="cb-3"></div>
+          <Link className="link" to="/products/1">
+            <button>Graphics Card</button>
+          </Link>
+        </div>
       </div>
       <div className="col col-l">
         <div className="row">
           <div className="col">
-            <div className="row">r4</div>
+            <div className="row">
+              <div className="cb-4"></div>
+              <Link className="link" to="/products/1">
+                <button>RAM</button>
+              </Link>
+            </div>
           </div>
           <div className="col">
-            <div className="row">r5</div>
+            <div className="row">
+              <div className="cb-5"></div>
+              <Link className="link" to="/products/1">
+                <button>Motherboard</button>
+              </Link>
+            </div>
           </div>
         </div>
-        <div className="row">r6</div>
+        <div className="row">
+          <div className="cb-6"></div>
+          <Link className="link" to="/products/1">
+            <button>Processor</button>
+          </Link>
+        </div>
       </div>
     </div>
   );
